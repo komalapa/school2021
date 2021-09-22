8c8e1b2b247ef178c0bca5a405a49a3760465163
komalapa <komalapa@gmail.com>
2021-09-22 23:43:43 +0400

    feat: modal form card-face

--- museum/js/ticketsForm.js
+++ museum/js/ticketsForm.js
@@ -41,3 +41,77 @@ function humanReadableDate(date){
 const testDate = new Date ('2022-08-19  16:30');
 document.querySelector('#overview-date').innerHTML = humanReadableDate(testDate)
 document.querySelector('#overview-time').innerHTML = testDate.getHours() + ':' + testDate.getMinutes()
+
+function cardNumberValidator(e){
+    if (e.key != 'Backspace') e.preventDefault();
+    // console.log(e.target.value.split(' ').join(''))
+    let val = e.target.value.split(' ').join('')
+    if (['1','2','3','4','5','6','7','8','9','0'].indexOf(e.key)>=0 && val.length < 16 ){
+        val += e.key;
+    }
+    e.target.value = `${val.slice(0,4)} ${val.slice(4,8)} ${val.slice(8,12)} ${val.slice(12)}`.trim()
+    
+}
+document.querySelector("#card-number").addEventListener('keydown', cardNumberValidator);
+
+function cardMonthValidator(e){
+    if (e.key != 'Backspace') e.preventDefault();
+    console.log(e.target.key)
+    let val = e.target.value
+    if (['1','2','3','4','5','6','7','8','9','0'].indexOf(e.key)>=0){
+        val += e.key;
+    }
+    val = +val
+    if (val < 0) {
+        e.target.value = '00'
+        return
+    }
+    if (val < 10) {
+        e.target.value = '0'+val
+        return
+    }
+    if (val < 13) {
+        e.target.value = val
+        return
+    }
+     e.target.value = '12'
+
+    
+}
+
+document.querySelector('#card-exp-m').addEventListener('keydown',cardMonthValidator)
+document.querySelector('#card-exp-m').addEventListener('input',cardMonthValidator)
+document.querySelector('#card-exp-m').addEventListener('change',cardMonthValidator)
+
+function cardYearValidator(e){
+    if (e.key != 'Backspace') e.preventDefault();
+    console.log(e.target.key)
+    let val = e.target.value
+    if (['1','2','3','4','5','6','7','8','9','0'].indexOf(e.key)>=0){
+        val += e.key;
+    }
+    val = +val
+    if (val < 2021) {
+        e.target.value = '2021'
+        return
+    }
+    if (val > 2999) {
+        e.target.value = '2999'
+        return
+    }
+    
+     e.target.value = val
+}
+document.querySelector('#card-exp-y').addEventListener('keydown',cardYearValidator)
+
+function stepper(that,direction){ //for using as onclick for step buttons in card form
+    // console.log(that)
+    const event = new Event('change');
+    if (direction === 'up'){
+        that.nextElementSibling.stepUp()
+        that.nextElementSibling.dispatchEvent(event)
+    } else {
+        that.previousElementSibling.stepDown()
+        that.previousElementSibling.dispatchEvent(event)
+    }
+}
