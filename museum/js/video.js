30e017ed1e58f570a4028a05b4bd0a425032bb94
komalapa <komalapa@gmail.com>
2021-09-25 15:02:19 +0400

    feat: slider in video section

--- museum/js/video.js
+++ museum/js/video.js
@@ -21,9 +21,10 @@ initVideoPlayer();
 
 
 function initVideoSlider(){
-  const videoContent = document.querySelector('.video-slides-wrp');
+
+  const videoContent = document.querySelector('.video-slider-wrp');
   const videoSliderControls = document.querySelector('.video-slider-controls');
-  const slides = videoContent.querySelectorAll('video');
+  const slides = videoContent.querySelectorAll('.video-slider-item');
   const controlsButtonsList = videoSliderControls.querySelector('.video-slider-controls-dots')
 
   const arrowLeft = videoSliderControls.querySelector('.arrow-left');
@@ -32,8 +33,8 @@ function initVideoSlider(){
   const sliderLength = slides.length;
   let activeVideo = 0;
   
-
-  const slidesWrp = videoContent;
+  videoContent.append(slides[0].cloneNode(true),slides[1].cloneNode(true),slides[2].cloneNode(true))
+  videoContent.prepend(slides[sliderLength -1].cloneNode(true),slides[sliderLength-2].cloneNode(true),slides[sliderLength-3].cloneNode(true))
 
   let inTransition = false;
   
@@ -42,9 +43,44 @@ function initVideoSlider(){
       sliderBtn.classList.add('video-slider-controls-dot');
       if (i === activeVideo)  sliderBtn.classList.add('active');
       sliderBtn.dataset.number = i;
-      // sliderBtn.addEventListener('click', ()=>changeSlide(+sliderBtn.dataset.number))
+      sliderBtn.addEventListener('click', ()=>setSliderOffset(i))
       controlsButtonsList.append(sliderBtn);
   }
+  const controlsBtns = controlsButtonsList.querySelectorAll('*');
+  function setSliderOffset(slideNumber){
+    if (typeof slideNumber !== 'undefined' ) {
+      controlsBtns[activeVideo].classList.remove('active');
+      activeVideo = slideNumber
+      controlsBtns[activeVideo].classList.add('active');
+    }
+    document.documentElement.style.setProperty('--video-slider-offset', `calc(${-activeVideo-3}*var(--video-slider-step))`);
+  }
 
+  
+  function moveSlider(direction = 'right'){
+    controlsBtns[activeVideo].classList.remove('active');
+    if (direction==='right'){
+      activeVideo++; 
+      setSliderOffset();
+    }else if (direction==='left'){
+      activeVideo--; 
+      setSliderOffset();
+    }
+    if (activeVideo < 0) activeVideo = sliderLength -1;
+    if (activeVideo > sliderLength-1) activeVideo =0;
+    controlsBtns[activeVideo].classList.add('active');
+  }
+  //eventlisteners
+  arrowLeft.addEventListener('click',()=>moveSlider('left'));
+  arrowRight.addEventListener('click',()=>moveSlider('right'));
+  videoContent.addEventListener('transitionstart', () => inTransition = true)
+  videoContent.addEventListener('transitionend', () => {
+      inTransition = false
+      videoContent.classList.add('no-transition');
+      setSliderOffset(activeVideo)
+      setTimeout(()=>{
+          videoContent.classList.remove('no-transition')
+      })
+  })
 }
 initVideoSlider()
