ad75c2f7a0e6e0ffc6063de227c57e8f201d94b2
komalapa <komalapa@gmail.com>
2021-12-06 11:56:41 +0400

    feat: change app for eslint, src ready for linter

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -4,18 +4,20 @@ import { AppView } from '../view/appView';
 
 class App {
   controller:AppController;
-  view: AppView;
-    constructor() {
-        this.controller = new AppController();
-        this.view = new AppView();
-    }
 
-    start() {
-        document
-            .querySelector('.sources')
-            ?.addEventListener('click', (e) => this.controller.getNews(e, (data:INewsData) => this.view.drawNews(data)));
-        this.controller.getSources((data:INewsData) => this.view.drawSources(data));
-    }
+  view: AppView;
+  
+  constructor() {
+    this.controller = new AppController();
+    this.view = new AppView();
+  }
+
+  start() {
+    document
+      .querySelector('.sources')
+      ?.addEventListener('click', (e) => this.controller.getNews(e, (data:INewsData) => this.view.drawNews(data)));
+    this.controller.getSources((data:INewsData) => this.view.drawSources(data));
+  }
 }
 
 export default App;
