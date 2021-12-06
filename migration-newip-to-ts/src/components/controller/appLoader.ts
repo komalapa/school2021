70ae4f7880e1e4d940022db1c8957c7d5fc68cf6
komalapa <komalapa@gmail.com>
2021-12-06 21:41:34 +0400

    fix: another link for access api

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', { // 'https://newsapi.org/v2/' default link doesn't work on gh-pages!
             apiKey: 'bdb3d3c1f2d44d208e71ace7ce56bd6c', // получите свой ключ https://newsapi.org/
         });
     }
