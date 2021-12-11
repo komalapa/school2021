67b24898c2ed3ad70f32da4c17d807ac0f4e9f16
komalapa <komalapa@gmail.com>
2021-12-11 13:56:33 +0400

    feat: toy class and assets

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,4 +1,8 @@
 import image from './images/lazy.png';
+import data from './data'
+import { Toy } from './toys/toy';
+// import { Shapes } from './types/types';
+
 
 const createImage = (src: string) => new Promise<HTMLImageElement>((res, rej) => {
   const img = new Image();
@@ -14,5 +18,17 @@ async function render() {
   document.body.appendChild(subHeader);
   document.body.appendChild(myImage);
 }
+// const set = new Set();
+// data.forEach(e => set.add(e.size))
+// console.log(set)
 
+// console.log(Shapes['шар'])
 render();
+
+// enum Colors {
+//   RED = "RED COLOR",
+//   BLUE = "BLUE COLOR",
+//   GREEN = "GREEN COLOR"
+// }
+// console.log(Object.keys(Colors).filter(key => Colors[key] === "RED COLOR"))
+ console.log (new Toy(data[0]))
---
+++ christmas-task/src/types/types.ts
@@ -0,0 +1,44 @@
+export enum Shapes{
+  Ball = 'шар',
+  Figure = 'фигурка',
+  Bell = 'колокольчик', 
+  Pinecone = 'шишка', 
+  Snowflake ='снежинка',
+  Other = '',
+}
+
+export enum Colors{
+  Yellow = 'желтый', 
+  Green = 'зелёный', 
+  Red = 'красный', 
+  White = 'белый', 
+  Blue = 'синий',
+  Other = 'другой',
+}
+
+export enum Size{
+  L = 'большой', 
+  M = 'средний', 
+  S = 'малый'
+}
+export interface IToy {
+  id: number,
+  name: string,
+  count: number,
+  year: number,
+  shape: Shapes,
+  color: Colors,
+  size: Size,
+  favorite: Boolean,
+}
+
+export type ToyData = {
+  num:string; 
+  name: string;
+  count: string; 
+  year: string; 
+  shape: string; 
+  color: string; 
+  size: string; 
+  favorite: boolean; 
+}
