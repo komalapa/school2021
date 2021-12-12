eb97511ae5296ab461196a2db946df51af5c27d1
komalapa <komalapa@gmail.com>
2021-12-12 13:30:45 +0400

    refactor: api key to the config file

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -1,9 +1,10 @@
+import { key } from '../../config';
 import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
         super('https://nodenews.herokuapp.com/', { // 'https://newsapi.org/v2/' default link doesn't work on gh-pages!
-            apiKey: 'bdb3d3c1f2d44d208e71ace7ce56bd6c', // получите свой ключ https://newsapi.org/
+            apiKey: key, // получите свой ключ https://newsapi.org/
         });
     }
 }
---
+++ migration-newip-to-ts/src/config.ts
@@ -0,0 +1,1 @@
+export const key:string = 'bdb3d3c1f2d44d208e71ace7ce56bd6c';
