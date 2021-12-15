8aa30e04255f685346cd33ddf8ea4c1ee79a298e
komalapa <komalapa@gmail.com>
2021-12-15 23:00:44 +0400

    feat: span range input component

--- christmas-task/src/App.tsx
+++ christmas-task/src/App.tsx
@@ -1,19 +1,22 @@
 // import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import { Hello } from './components/hello'
-import { Toy } from './types/toys/toy'
-import { data } from './data'
-import { ToyCard } from './components/ToyCard/ToyCard';
-import { ToysContainter } from './components/ToysContainer/ToysContainer'
+import { Toy } from "./types/toys/toy";
+import { data } from "./data";
+import { ToyCard } from "./components/ToyCard/ToyCard";
+import { ToysContainter } from "./components/ToysContainer/ToysContainer";
+import { SpanInput } from "./components/SpanInput/SpanInput";
 
-const toysArray : Array<Toy> = data.map(item => new Toy(item));
+const toysArray: Array<Toy> = data.map((item) => new Toy(item));
 
 function App() {
-  const testToy:Toy = new Toy(data[0]);
+  const testToy: Toy = new Toy(data[0]);
   return (
     <div className="App">
-      <ToysContainter toys={toysArray}/>
+      <ToysContainter toys={toysArray} />
+
+      <SpanInput max={10} min={0} step={1} />
     </div>
   );
 }
---
+++ christmas-task/src/components/SpanInput/SpanInput.tsx
@@ -0,0 +1,54 @@
+// import "./FavoriteButton.css";
+import { ReactComponent as HeartIcon } from "../../assets/svg/heart.svg";
+import { ReactComponent as HeartIconFull } from "../../assets/svg/heart-full.svg";
+import { FormEventHandler, useState } from "react";
+import "./SpanInput.css";
+
+type SpanInputProps = {
+  min: number;
+  max: number;
+  step: number;
+};
+
+export function SpanInput(props: SpanInputProps) {
+  // let isFavorite = props.isFavorite;
+  const [minValue, setMinValue] = useState(props.min);
+  const [maxValue, setMaxValue] = useState(props.max);
+
+  function handleMinInput(e: React.FormEvent<HTMLInputElement>): void {
+    console.log(e);
+    const curValue: number = +e.currentTarget.value;
+    if (curValue < maxValue) setMinValue(curValue);
+  }
+
+  function handleMaxInput(e: React.FormEvent<HTMLInputElement>): void {
+    console.log(e);
+    const curValue: number = +e.currentTarget.value;
+    if (curValue > minValue) setMaxValue(curValue);
+  }
+  // console.log(props);
+  return (
+    <div className="span-input">
+      <span className="span-input__text span-input__min-text">{minValue}</span>
+      <input
+        className="span-input__min"
+        type="range"
+        min={props.min}
+        max={props.max}
+        step={props.step}
+        value={minValue}
+        onInput={handleMinInput}
+      />
+      <input
+        className="span-input__max"
+        type="range"
+        min={props.min}
+        max={props.max}
+        step={props.step}
+        value={maxValue}
+        onInput={handleMaxInput}
+      />
+      <span className="span-input__max-text span-input__text">{maxValue}</span>
+    </div>
+  );
+}
