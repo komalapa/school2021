6df476b085e541ab3d6b6028deaf0786fc061a44
komalapa <komalapa@gmail.com>
2021-12-19 00:48:31 +0400

    feat: size  filter from props

--- christmas-task/src/components/SizeFilter/SizeFilter.tsx
+++ christmas-task/src/components/SizeFilter/SizeFilter.tsx
@@ -1,6 +1,7 @@
 import { Size } from "../../types/types";
 import { ReactComponent as BallIcon } from "../../assets/svg/ball-2.svg";
 import "./SizeFilter.css";
+import { useState } from "react";
 
 type SizeFilterContainerProps = {
   toggleFilter: CallableFunction;
@@ -8,6 +9,9 @@ type SizeFilterContainerProps = {
 };
 
 export function SizeFilter(props: SizeFilterContainerProps) {
+  const [isS, setIsS] = useState(props.checked.indexOf(Size.S) >= 0);
+  const [isM, setIsM] = useState(props.checked.indexOf(Size.M) >= 0);
+  const [isL, setIsL] = useState(props.checked.indexOf(Size.L) >= 0);
   return (
     <div className="size-filter">
       <h3 className="size-filter__header">Размер</h3>
@@ -15,8 +19,11 @@ export function SizeFilter(props: SizeFilterContainerProps) {
         <input
           className="size-filter__check"
           type="checkbox"
-          onInput={() => props.toggleFilter("size", Size.L)}
-          // checked={props.checked.indexOf(Size.L) >= 0}
+          onInput={() => {
+            setIsL(!isL);
+            props.toggleFilter("size", Size.L);
+          }}
+          checked={isL}
         />
         <BallIcon className="size-filter__icon size-filter__icon-l" />
       </label>
@@ -24,8 +31,11 @@ export function SizeFilter(props: SizeFilterContainerProps) {
         <input
           className="size-filter__check"
           type="checkbox"
-          onInput={() => props.toggleFilter("size", Size.M)}
-          // checked={props.checked.indexOf(Size.M) >= 0}
+          onInput={() => {
+            setIsM(!isM);
+            props.toggleFilter("size", Size.M);
+          }}
+          checked={isM}
         />
         <BallIcon className="size-filter__icon size-filter__icon-m" />
       </label>
@@ -33,8 +43,11 @@ export function SizeFilter(props: SizeFilterContainerProps) {
         <input
           className="size-filter__check"
           type="checkbox"
-          onInput={() => props.toggleFilter("size", Size.S)}
-          // checked={props.checked.indexOf(Size.S) >= 0}
+          onInput={() => {
+            setIsS(!isS);
+            props.toggleFilter("size", Size.S);
+          }}
+          checked={isS}
         />
         <BallIcon className="size-filter__icon size-filter__icon-s" />
       </label>
