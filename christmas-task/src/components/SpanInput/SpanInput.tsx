62598586c92cf8182e3bc16b27113ba343e09696
komalapa <komalapa@gmail.com>
2021-12-19 22:07:54 +0400

    feat: colored trac for span inputs.

--- christmas-task/src/components/SpanInput/SpanInput.tsx
+++ christmas-task/src/components/SpanInput/SpanInput.tsx
@@ -15,6 +15,9 @@ export function SpanInput(props: SpanInputProps) {
   const [maxValue, setMaxValue] = useState(props.maxVal || props.max);
   const [isSended, setIsSended] = useState(false); //for send span value only on next render
 
+  const [gradientValue, setGradientValue] = useState<string>(
+    "to right, white, white 0%, gold 0%, gold 100%, white 100%, white 100%"
+  );
   function handleMinInput(e: React.FormEvent<HTMLInputElement>): void {
     const curValue: number = +e.currentTarget.value;
     if (curValue <= maxValue) setMinValue(curValue);
@@ -30,9 +33,26 @@ export function SpanInput(props: SpanInputProps) {
   if (!isSended) {
     props.toggleFilter(minValue, maxValue);
     setIsSended(true);
+    gradient();
   }
+
+  function gradient() {
+    let step = (props.step * 100) / (props.max - props.min);
+    let start = ((minValue - props.min) / props.step) * step;
+    let end = ((maxValue - props.min) / props.step) * step;
+    setGradientValue(
+      `to right, white, white ${start}%, gold ${start}%, gold ${end}%, white ${end}%, white 100%`
+    );
+    console.log(start, end, step);
+  }
+
   return (
     <div className="span-input">
+      <span
+        className="span-input__track"
+        style={{ backgroundImage: `linear-gradient(${gradientValue})` }}
+      ></span>
+
       <span className="span-input__text span-input__min-text">{minValue}</span>
       <input
         className="span-input__min"
