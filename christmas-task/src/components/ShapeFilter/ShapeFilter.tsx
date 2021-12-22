251abb329dda3f6ccba3fea37522f0ea44a5839b
komalapa <komalapa@gmail.com>
2021-12-22 23:13:15 +0400

    refactor: shapes filter

--- christmas-task/src/components/ShapeFilter/ShapeFilter.tsx
+++ christmas-task/src/components/ShapeFilter/ShapeFilter.tsx
@@ -5,90 +5,72 @@ import { ReactComponent as FigureIcon } from "../../assets/svg/toy.svg";
 import { ReactComponent as PineIcon } from "../../assets/svg/pine.svg";
 import { ReactComponent as SnowIcon } from "../../assets/svg/snowflake.svg";
 
-import { useState } from "react";
+import { FC } from "react";
 
 import "./ShapeFilter.css";
+import { FilterCheckInput } from "../FilterCheckInput/FilterCheckInput";
 
 type ShapeFilterContainerProps = {
   toggleFilter: CallableFunction;
   checked: Shapes[];
 };
 
-export function ShapeFilter(props: ShapeFilterContainerProps) {
-  const [isBall, setIsBall] = useState(props.checked.indexOf(Shapes.Ball) >= 0);
-  const [isBell, setIsBell] = useState(props.checked.indexOf(Shapes.Bell) >= 0);
-  const [isFigure, setIsFigure] = useState(
-    props.checked.indexOf(Shapes.Figure) >= 0
-  );
-  const [isPine, setIsPine] = useState(
-    props.checked.indexOf(Shapes.Pinecone) >= 0
-  );
-  const [isSnow, setIsSnow] = useState(
-    props.checked.indexOf(Shapes.Snowflake) >= 0
-  );
+export const ShapeFilter: FC<ShapeFilterContainerProps> = (props) => {
+  const { toggleFilter, checked } = props;
+  function handleFilter(filterName, filterValue) {
+    toggleFilter(filterName, filterValue);
+  }
   return (
     <div className="shape-filter">
-      <span className="shape-filter__header">Формы</span>
-      <label className="shape-filter__lbl">
-        <input
-          className="shape-filter__check"
-          type="checkbox"
-          onInput={() => {
-            setIsBall(!isBall);
-            props.toggleFilter("shape", Shapes.Ball);
-          }}
-          defaultChecked={isBall}
-        />
+      <h3 className="shape-filter__header">Формы</h3>
+      <FilterCheckInput
+        className="shape-filter"
+        onToggleFilter={handleFilter}
+        filterName="shape"
+        filterValue={Shapes.Ball}
+        checked={checked.indexOf(Shapes.Ball) >= 0}
+      >
         <BallIcon className="shape-filter__icon" />
-      </label>
-      <label className="shape-filter__lbl">
-        <input
-          className="shape-filter__check"
-          type="checkbox"
-          onInput={() => {
-            setIsBell(!isBell);
-            props.toggleFilter("shape", Shapes.Bell);
-          }}
-          defaultChecked={isBell}
-        />
+      </FilterCheckInput>
+      <FilterCheckInput
+        className="shape-filter"
+        onToggleFilter={handleFilter}
+        filterName="shape"
+        filterValue={Shapes.Bell}
+        checked={checked.indexOf(Shapes.Bell) >= 0}
+      >
         <BellIcon className="shape-filter__icon" />
-      </label>
-      <label className="shape-filter__lbl">
-        <input
-          className="shape-filter__check"
-          type="checkbox"
-          onInput={() => {
-            setIsFigure(!isFigure);
-            props.toggleFilter("shape", Shapes.Figure);
-          }}
-          defaultChecked={isFigure}
-        />
+      </FilterCheckInput>
+
+      <FilterCheckInput
+        className="shape-filter"
+        onToggleFilter={handleFilter}
+        filterName="shape"
+        filterValue={Shapes.Figure}
+        checked={checked.indexOf(Shapes.Figure) >= 0}
+      >
         <FigureIcon className="shape-filter__icon" />
-      </label>
-      <label className="shape-filter__lbl">
-        <input
-          className="shape-filter__check"
-          type="checkbox"
-          onInput={() => {
-            setIsPine(!isPine);
-            props.toggleFilter("shape", Shapes.Pinecone);
-          }}
-          defaultChecked={isPine}
-        />
+      </FilterCheckInput>
+
+      <FilterCheckInput
+        className="shape-filter"
+        onToggleFilter={handleFilter}
+        filterName="shape"
+        filterValue={Shapes.Pinecone}
+        checked={checked.indexOf(Shapes.Pinecone) >= 0}
+      >
         <PineIcon className="shape-filter__icon" />
-      </label>
-      <label className="shape-filter__lbl">
-        <input
-          className="shape-filter__check"
-          type="checkbox"
-          onInput={() => {
-            setIsSnow(!isSnow);
-            props.toggleFilter("shape", Shapes.Snowflake);
-          }}
-          defaultChecked={isSnow}
-        />
+      </FilterCheckInput>
+
+      <FilterCheckInput
+        className="shape-filter"
+        onToggleFilter={handleFilter}
+        filterName="shape"
+        filterValue={Shapes.Snowflake}
+        checked={checked.indexOf(Shapes.Snowflake) >= 0}
+      >
         <SnowIcon className="shape-filter__icon" />
-      </label>
+      </FilterCheckInput>
     </div>
   );
-}
+};
