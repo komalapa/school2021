f7c889c4b83e2a8fbcb83ba7d506405fd1848121
komalapa <komalapa@gmail.com>
2022-01-20 09:19:05 +0400

    feat: pagination

--- acync-race/acync-race/src/components/garage/garage.tsx
+++ acync-race/acync-race/src/components/garage/garage.tsx
@@ -3,14 +3,16 @@ import { add100Cars, getCars } from "../../api/garage";
 import { Car } from "../../types/api-response";
 import CarView from "../carView/carView";
 import EditCarForm from "../editCarForm/editCarForm";
+import Pagination from "../pagination/pagination";
 
 const Garage: FC = () => {
   const [isGarageChanged, setIsGarageChanged] = useState<boolean>(true);
   const [cars, setCars] = useState<Car[]>([]);
   const [carsCount, setCarsCount] = useState<number>(0);
+  const [curPage, setCurPage] = useState<number>(1);
 
   if (isGarageChanged) {
-    getCars(1).then((data) => {
+    getCars(curPage).then((data) => {
       setCars(data.cars);
       setCarsCount(data.count);
     });
@@ -30,11 +32,22 @@ const Garage: FC = () => {
     });
   }
 
+  function handleChangePage(direction: string) {
+    if (direction === "next") setCurPage(curPage + 1);
+    if (direction === "prev") setCurPage(curPage - 1);
+    setIsGarageChanged(true);
+  }
+
   return (
     <div className="garage">
       <EditCarForm onCarInput={handleCarInput} />
       <button onClick={handleAdd100}>Add 100 cars</button>
       {carEls}
+      <Pagination
+        page={curPage}
+        onChange={handleChangePage}
+        count={carsCount}
+      />
     </div>
   );
 };
---
+++ acync-race/acync-race/src/components/pagination/pagination.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState } from "react";
+import { carsPerPage } from "../../constants";
+
+import "./pagination.css";
+
+interface PaginationProps {
+  page?: number;
+  count: number;
+  onChange: CallableFunction;
+}
+
+const Pagination: FC<PaginationProps> = ({ page, count, onChange }) => {
+  const pagesCount = Math.ceil(count / carsPerPage);
+
+  const [curPage, setCurPage] = useState<number>(
+    page ? (page < 0 ? pagesCount : page) : 1
+  );
+
+  function changePage(direction: string) {
+    onChange(direction);
+    if (direction === "next") setCurPage(curPage + 1);
+    if (direction === "prev") setCurPage(curPage - 1);
+  }
+
+  if (curPage > pagesCount) changePage("prev");
+
+  return (
+    <div className="pagination">
+      <button
+        className="pagination-button"
+        onClick={() => changePage("prev")}
+        disabled={curPage <= 1}
+      >
+        prev
+      </button>
+      <span className="pagination-page">
+        {curPage}/{pagesCount}
+      </span>
+      <button
+        className="pagination-button"
+        onClick={() => changePage("next")}
+        disabled={curPage >= pagesCount}
+      >
+        next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
